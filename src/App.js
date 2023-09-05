85114e70a0135c3d0cef181316cca2c1b39b9dc2
codeemaker <jgopinathjayakumar@gmail.com>
2023-09-05 09:28:53 +0530

    stripe frontend created

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const checkout = () => {
+      fetch("http://localhost:5000/api/create-payment-intent", {
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          items:[
+            {id:1,quantity:1,price:(200.00 * 100),name:"Test"}
+          ]
+        })
+      })
+      .then(res => {
+        if (res.ok) return res.json()
+        return res.json().then(json => Promise.reject(json))
+      })
+      .then(({url})=>{
+        console.log(url)
+        window.location = url
+      })
+      .catch(e => {
+        console.log(e.error)
+      })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={checkout}>Checkout</button>
     </div>
   );
 }
